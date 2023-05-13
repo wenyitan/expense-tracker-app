d09433217c8a1590f346d93319adbc94b2e3179e
wenyitan <tanwenyi1511@gmail.com>
2023-05-13 22:33:20 +0800

    added weight tracker page router

--- wens-page-react/src/App.js
+++ wens-page-react/src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LogTransaction from "./pages/LogTransaction";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Nav/Navbar";
+import WeightTracker from './pages/WeightTracker';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/log-transactions" element={<LogTransaction />} />
+        <Route path="/weight-tracker" element={<WeightTracker />} />
       </Routes>
     </Router>
   );
