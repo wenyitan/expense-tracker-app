ee9177b7bac7459b4931b1756ab0180eef1f08a8
wenyitan <tanwenyi1511@gmail.com>
2023-07-01 20:48:59 +0800

    removed top directory

--- startExpenseApp.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# To start project
-cd /Users/wenyi/ProjectWorkspace/WebPageApp/expense-tracker-python
-nohup uvicorn expense_tracker_app:app --reload --port 8000 &
-echo "Python app started"
--- startWeightApp.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# To start project
-cd /Users/wenyi/ProjectWorkspace/WebPageApp/weight-tracker-python
-nohup uvicorn weight_tracker_app:app --reload --port 8001 &
-echo "Python app started"
